e1040e34068a6d6af95f1323348191dea1439c15
MoleSter <staricmaggacet@mail.ru>
2020-09-01 02:37:14 +0300

    homework2

---
+++ Homework_GB_2/AmountOfNum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmountOfNum
+{
+    class Program
+    {   //      Написать метод подсчета количества цифр числа.
+        //      Комышан Виктор
+        static void Main(string[] args)
+        {
+            int a = 1111;
+            int count = 1;
+            while( a > 9)
+            {
+                a = a / 10;
+                count++;
+            }
+            Console.WriteLine(count);
+            Console.Read();
+
+        }
+    }
+}
