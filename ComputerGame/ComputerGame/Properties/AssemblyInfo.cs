5873a74927e621634ad49cb565f1ab8c88294df9
MoleSter <staricmaggacet@mail.ru>
2020-09-29 14:31:27 +0300

    homework2.1

---
+++ ComputerGame/ComputerGame/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ComputerGame")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("ComputerGame")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("89cdc5a9-32b8-4e43-9032-9ec21727fb76")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ComputerGame/ComputerGame/Universe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+
+namespace ComputerGame
+{
+    class Universe : BaseObject
+    {
+        public Universe(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+        }
+        public override void Draw()
+        {
+            Image newImage = Image.FromFile("1.png");
+            Game.Buffer.Graphics.DrawImage(newImage, Pos.X, Pos.Y);
+        }
+        public override void Update()
+        {
+            Pos.X = Pos.X - Dir.X;
+            if (Pos.X < 0) Pos.X = Game.Width;
+            if (Pos.X > Game.Width) Pos.X = 0;
+        }
+    }
+}
